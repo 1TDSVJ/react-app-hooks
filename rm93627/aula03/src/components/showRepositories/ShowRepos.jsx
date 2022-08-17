94cf397d68907498b5cabea9c5ec0be11bc3dfff
rm93627 <rm93627@fiap.com.br>
2022-08-17 11:57:33 -0300

    :sparkles: feat: Terceiro Commit

--- rm93627/aula03/src/components/showRepositories/ShowRepos.jsx
+++ rm93627/aula03/src/components/showRepositories/ShowRepos.jsx
@@ -1,19 +1,31 @@
 import React from "react";
+import { useEffect } from "react";
 
 const ShowRepos = (props) => {
-  let estilo;
+  //useState
+  //   let estilo;
 
-  if (new Date().getHours() < 12) {
-    estilo = props.objEstilos.manha;
-  } else if (new Date().getHours() > 12 && new Date().getHours() < 18) {
-    estilo = props.objEstilos.tarde;
-  } else {
-    estilo = props.objEstilos.noite;
-  }
+  //   if (new Date().getHours() < 12) {
+  //     estilo = props.objEstilos.manha;
+  //   } else if (new Date().getHours() > 12 && new Date().getHours() < 18) {
+  //     estilo = props.objEstilos.tarde;
+  //   } else {
+  //     estilo = props.objEstilos.noite;
+  //   }
+
+  useEffect(async () => {
+
+    const response = await fetch("https://api.github.com/users")
+    const data = await response.json()
+    props.setRepositorios(data)
+    
+  }, []);
+
+  useEffect(() => console.log("Renderizou o repositório"), [props.repositorios]);
 
   return (
     <div>
-      <h2 style={estilo}>Lista de Repositórios</h2>
+      <h2>Lista de Repositórios</h2>
       <p>{props.repositorios}</p>
       <button onClick={() => props.setRepositorios("Alterado no filho")}>
         Alterar
