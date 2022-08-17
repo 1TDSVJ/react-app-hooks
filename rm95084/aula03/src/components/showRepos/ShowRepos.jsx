7e3d5eec79f4f0b841f46e120fd08d85e07b3cfd
LucasGuerraS <lucasgasardo@gmail.com>
2022-08-17 12:02:27 -0300

    Commit

--- rm95084/aula03/src/components/showRepos/ShowRepos.jsx
+++ rm95084/aula03/src/components/showRepos/ShowRepos.jsx
@@ -6,6 +6,7 @@ const ShowRepos = (props) => {
     const fecth = async () => {
       const response = await fetch("https://api.github.com/users");
       const data = await response.json();
+      return data
     };
     fecth();
   }, []);
