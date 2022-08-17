753b3f8c702bdfbbf9fa8aca3d06b56942abfe29
DaniHenriqueAOM <daniel.h.a.o.m@gmail.com>
2022-08-17 11:59:44 -0300

    useEffect com problema kk

--- rm94615/Aula_03/src/components/showRepos/ShowRepos.jsx
+++ rm94615/Aula_03/src/components/showRepos/ShowRepos.jsx
@@ -1,26 +1,39 @@
 import React from "react"
+import { useEffect } from "react"
 
 export default function ShowRepos(props){
 
-    let estilo
+    // let estilo
 
-    if(new Date().getHours() < 12){
-        estilo = props.objEstilos.manha
+    // if(new Date().getHours() < 12){
+    //     estilo = props.objEstilos.manha
         
-    } else if(new Date().getHours() > 12 && new Date().getHours() < 18){
-        estilo = props.objEstilos.tarde
-    } else{
-        estilo = props.objEstilos.noite
-    }
+    // } else if(new Date().getHours() > 12 && new Date().getHours() < 18){
+    //     estilo = props.objEstilos.tarde
+    // } else{
+    //     estilo = props.objEstilos.noite
+    // }
+
+    useEffect(async ()=>{
+        
+        const response = await fetch("https://api.github.com/users/")
+        const data = await response.json()
+        props.setRepositorios(data)
+
+    },[])
+
+    useEffect(()=>{
+        console.log("Renderizou o repositório")
+    },[props.repositorios])
 
     return(
         <div>
-            <h2 style={estilo}>Lista de repositórios</h2>
+            <h2>Lista de repositórios</h2>
             <p>{props.repositorios}</p>
 
             <button onClick={()=> props.setRepositorios("Alterado no filho!")}>Alterar</button>
             
-
+            
 
         </div>
     )
