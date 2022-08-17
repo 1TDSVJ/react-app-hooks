c200b0f1c9be62ddf21e3883f057dc0d7b8eb85f
LeonardoRCosta <rm94286@fiap.com.br>
2022-08-17 09:43:34 -0300

    Componente Repositorios importado no App.jsx

---
+++ rm94286/aula03/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Repositorios from './components/repositorios/Repositorios';
+
+export default function App() {
+  return (
+    <>
+      <h1>Estudos sobre HOOKS</h1>
+      <Repositorios />
+    </>
+  );
+}
