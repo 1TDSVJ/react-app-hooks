9233b3fe4ea48a8b588dfb8ae93f72831b518fa1
heitorborbamarini <rm92976@fiap.com.br>
2022-08-17 11:59:38 -0300

    Ale perdeu a batalha

--- rm92976/aula03/src/components/showRepos/ShowRepos.jsx
+++ rm92976/aula03/src/components/showRepos/ShowRepos.jsx
@@ -1,21 +1,27 @@
 import React from "react";
+import {useEffect} from "react";
 
 export default function ShowRepos(props) {
 
-    let estilo
-    console.log(new Date().getHours())
     
-        if(new Date().getHours() < 12 ){
-            estilo = props.objEstilos.manha
-        } else if(new Date().getHours() > 12 && new Date().getHours() < 18){
-            estilo = props.objEstilos.tarde
-        } else{
-            estilo = props.objEstilos.noite
 
-        }
+    
+    useEffect(async () => {
+
+     const response = await fetch('https://api.github.com/users')
+     const data = await response.json()
+     props.setRepositorios(data)
+
+
+    },[])
+
+    useEffect(() => {
+        console.log('RENDERIZOU O REPOSITORIO')
+    },[props.repositorios, ])
+
     return(
         <div>
-            <h2 style={estilo}>Lista de Repositórios</h2>
+            <h2 >Lista de Repositórios</h2>
             <p>{props.repositorios}</p>
             <button onClick={()=> props.setRepositorios("Alterado no Filho!")}> ALTERAR</button>
         </div>
