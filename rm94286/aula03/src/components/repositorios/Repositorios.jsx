5848fb5865facf5b6bd294901b5fc77a1a339670
LeonardoRCosta <rm94286@fiap.com.br>
2022-08-17 09:44:08 -0300

    Componente Repositorios criado.

---
+++ rm94286/aula03/src/components/repositorios/Repositorios.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export default function Repositorios() {
+  const [repositorios, setRepositorios] = useState();
+  return (
+    <div>
+      <h2>Exemplo de utilização do useState</h2>
+      <p>Meu repositório: {repositorios}</p>
+      <button onClick={() => setRepositorios('1TDSVJ')}>Add-Repo</button>
+    </div>
+  );
+}
