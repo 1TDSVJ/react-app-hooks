2839f73ac5fa60c5960b7af4d1236935f0c715fb
GuWuelta <rm93449@fiap.com.br>
2022-08-17 09:42:52 -0300

    App basico react + exemplo 1 useState.

---
+++ rm93449/aula03/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Repositorios from "./components/repositorios/Repositorios"
+
+export default function App(){
+    return(
+        <div>
+            <h1>ESTUDO SOBRE HOOKS</h1>
+            <Repositorios/>
+        </div>
+    )
+}
