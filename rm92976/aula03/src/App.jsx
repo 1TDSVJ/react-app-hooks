03c47e06e6817a277cfac4a0008b119a102b42f1
heitorborbamarini <rm92976@fiap.com.br>
2022-08-17 09:42:45 -0300

    App Básico react + Exemplo 1 useState

---
+++ rm92976/aula03/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Repositorios from "./components/repositorios/Repositorios";
+
+export default function App() {
+  return (
+    <div>
+        <h1>ESTUDOS SOBRE HOOKS </h1>
+        <Repositorios/>
+
+    </div>
+  );
+}
