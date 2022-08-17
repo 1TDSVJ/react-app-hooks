5426386297eb781bc80088394a5519b147fc65b4
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-17 09:42:50 -0300

    Componente Repositorios criado

---
+++ rm94281/aula03/src/components/repositorios/Repositorios.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export default function Repositorios() {
+    const [ repositorios, setRepositorios ] = useState();
+    return(
+        <>
+            <h2>Exemplo de utilização de useState</h2>
+            <p>Meu repositório: {repositorios}</p>
+            <button onClick={() => setRepositorios("1TDSVJ")}>Add-Repo</button>
+        </>
+    );
+}
