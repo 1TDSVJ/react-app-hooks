fccf264b0fd6f696184c4581d86d5f7a307aa971
MateusLem <rm93480@fiap.com.br>
2022-08-17 09:15:30 -0300

    Aula 03 do Intensivo de REACT

---
+++ rm93480/aula03/src/App.jsx
@@ -0,0 +1,25 @@
+//rfc -> cria estrutura básica
+
+import React from "react"
+
+//export default function App(){
+//    return()
+//}
+
+export default function App(){
+    return(
+        <div>
+            <h1>ESTUDO SOBRE HOOKS</h1>
+
+            
+        </div>
+    )
+}
+
+//const App = () => {
+//    return(
+//        <>
+//        
+//        </>
+//   )
+//}
