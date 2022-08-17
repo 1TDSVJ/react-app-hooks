f111333fea8bd727ac28a2bb9bc8b2aa197c349a
ncalegari <rm93912@fiap.com.br>
2022-08-17 09:42:54 -0300

    App basico react + Exemplo 1 useState

---
+++ rm93912/aula03/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Repositorios from "./components/repositorios/Repositorios"
+
+// const App = () =>{
+//     return(
+//         <div>
+
+//         </div>
+//     )
+// }
+
+// export default App
+
+export default function App(){
+    return(
+        <div>
+            <h1>ESTUDO SOBRE HOOKS</h1>
+            <Repositorios/>
+        </div>
+    )
+}
