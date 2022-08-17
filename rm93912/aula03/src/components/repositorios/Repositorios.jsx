d0c293406088e3488bfa10289ed349e1f2749df5
ncalegari <rm93912@fiap.com.br>
2022-08-17 11:22:00 -0300

    Exemplos de useState com css em obj e op condicionais

--- rm93912/aula03/src/components/repositorios/Repositorios.jsx
+++ rm93912/aula03/src/components/repositorios/Repositorios.jsx
@@ -1,15 +1,39 @@
 import React, {useState} from "react"
+import ShowRepos from "../showRepos/ShowRepos"
 
 export default function Repositorios(){
 
-    const [repositorios, setRepositorios] = useState()
+    const objEstilos = {
+        noite:{backgroundColor:'#ff0000'},
+        tarde:{backgroundColor:'#fff000'},
+        manha:{backgroundColor:'#0000ff'}
+    }
+
+    const [repositorios, setRepositorios] = useState("1TDSVJ")
+    let valorVariavel = "2022"
+
+    const alteraRepo = () =>{
+        setRepositorios(repositorios + 1)
+        valorVariavel += 5
+        console.log(valorVariavel)
+    }
 
     return(
 
         <div>
-            <h2>Exemplo de utilização do useState</h2>
-            <p>Meu repositório : {repositorios}</p>
-            <button onClick={()=> setRepositorios("1TDSVJ")}>Add-Repo</button>
+            <h2 style={{backgroundColor:'#00ff00',color:'#ff0000', textAlign:'center'}}>Exemplo de utilização do useState</h2>
+            <p style={objEstilos.noite}>Meu repositório : {repositorios}</p>
+            <p style={objEstilos.manha}>Minha Variável: {valorVariavel}</p>
+
+            <button onClick={()=> alteraRepo()}>Altera Dados</button>
+
+            <ShowRepos
+                repositorios={repositorios} 
+                setRepositorios={setRepositorios}
+                objEstilos={objEstilos}
+            />
+            
+
         </div>
 
     )
---
+++ rm93912/aula03/src/components/showRepos/ShowRepos.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function ShowRepos(props){
+
+    let estilo
+        
+        if(new Date().getHours() < 12){
+            estilo = props.objEstilos.manha
+        }else if(new Date().getHours() > 12 && new Date().getHours() < 18){
+            estilo = props.objEstilos.tarde
+        }else{
+            estilo = props.objEstilos.noite
+        }
+
+    return(
+        <div>
+            <h2 style={estilo}>Lista de Repositórios</h2>
+            <p>{props.repositorios}</p>
+            <button onClick={()=> props.setRepositorios("ALTERADO NO FILHO!")}>ALTERAR</button>
+
+
+        </div>
+    )
+}
