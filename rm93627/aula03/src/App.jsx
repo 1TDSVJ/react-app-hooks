e83eaece3eb057fe0a363667258644ff7dbe3a32
rm93627 <rm93627@fiap.com.br>
2022-08-17 09:43:47 -0300

    :tada : First Commit

---
+++ rm93627/aula03/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Repositorios from "./components/repositories/Repositorios";
+
+const App = () => (
+  <div>
+    <h1>
+        Estudos de React
+    </h1>
+    <Repositorios/>
+  </div>
+);
+
+export default App;
---
+++ rm93627/aula03/src/components/repositories/Repositorios.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Repositorios = () => {
+  const [repositorios, setRepositorios] = useState();
+
+  return (
+  <div>
+    <h2>Exemplo de utilização do useState</h2>
+    <p>Meu repositório: {repositorios}</p>
+    <button onClick={() => setRepositorios("1TDSVJ")}>Add-Repo</button>
+  </div>
+  )
+  
+};
+
+export default Repositorios;
