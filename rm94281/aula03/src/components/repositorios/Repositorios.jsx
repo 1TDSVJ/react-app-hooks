bc7651c2b150f414755c7abdf7e325aef33540d4
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-17 09:53:19 -0300

    exemplo 1 finalizado.

--- rm94281/aula03/src/components/repositorios/Repositorios.jsx
+++ rm94281/aula03/src/components/repositorios/Repositorios.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 
 export default function Repositorios() {
-    const [ repositorios, setRepositorios ] = useState();
+    const [ repositorios, setRepositorios ] = useState("1TDSVJ");
+    let valorVariavel = "2022";
+
+    const alteraRepo = () => {
+        setRepositorios(repositorios + 1);
+        valorVariavel += 5;
+        console.log(valorVariavel);
+    }
+
     return(
         <>
             <h2>Exemplo de utilização de useState</h2>
             <p>Meu repositório: {repositorios}</p>
-            <button onClick={() => setRepositorios("1TDSVJ")}>Add-Repo</button>
+            <p>Minha variável: {valorVariavel}</p>
+            <button onClick={() => alteraRepo("1TDSVJ")}>Add-Repo</button>
         </>
     );
 }
