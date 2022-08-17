f9d7c0b1da1e7fab56786909f2fc31a0529a8ebb
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-17 09:42:31 -0300

    Componente App criado.

---
+++ rm94281/aula03/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Repositorios from './components/repositorios/Repositorios'
+
+export default function App() {
+  return (
+    <>
+      <h1>Estudos sobre hooks</h1>
+      <Repositorios />
+    </>
+  )
+}
+
