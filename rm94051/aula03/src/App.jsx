890875c9f71ce842af9c9cced0389d3e0f80e6e9
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2022-08-17 09:42:55 -0300

    App básico react + exemplo1 + useState!

---
+++ rm94051/aula03/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Repositorios from "./components/repositorios/Repositorios";
+
+export default function App(){
+    return (
+        <div>
+            <h1>ESTUDO SOBRE HOOKS</h1>
+            <Repositorios/>
+
+        </div>
+    )
+}
+
+// const App = () => {
+//     return(
+//         <>
+        
+//         </>
+//     )
+// }
+
+// export default App
