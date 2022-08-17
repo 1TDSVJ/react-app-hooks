88d34291da26bde979b573b4c0ccc3a80e710da4
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2022-08-17 11:22:06 -0300

    Exemplos  de useState css em Obj e Op Condicionais

--- rm94051/aula03/src/components/repositorios/Repositorios.jsx
+++ rm94051/aula03/src/components/repositorios/Repositorios.jsx
@@ -1,15 +1,38 @@
 import React, {useState} from "react";
+import ShowRepos from "../showRepos/ShowRepos";
 
 export default function Repositorios() {
 
-    const [repositorios, setRepositorios] = useState()
+    const objEstilos = {
+        noite: {backgroundColor:"#FF0000"},
+        tarde: {backgroundColor:"#FFF000"},
+        manha: {backgroundColor:"#0000FF"}
+    }
+
+    const [repositorios, setRepositorios] = useState("1TDSVJ")
+    let valorVariavel = "2022"
+
+    const alteraRepo = ()=>{
+        setRepositorios(repositorios + 1)
+        valorVariavel += 5
+        console.log(valorVariavel)
+    }
 
 
     return (
         <div>
-            <h2>Exemplo de utilização do useState</h2>
-            <p>Meu repositório : {repositorios}</p>
-            <button onClick={() => setRepositorios("1TDSVJ")}>Add-Repo</button>
+            <h2 style={{backgroundColor:"#00FF00",color:"#FF0000", textAlign:"center"}}>Exemplo de utilização do useState</h2>
+            <p style={objEstilos.noite}>Meu repositório : {repositorios}</p>
+            <p style={objEstilos.manha}>Minha Variável: {valorVariavel}</p>
+
+            <button onClick={() => alteraRepo()}>Altera Dados</button>
+
+            <ShowRepos 
+                repositorios = {repositorios} 
+                setRepositorios = {setRepositorios}
+                objEstilos={objEstilos}
+                />
+            <button onClick={() => alteraRepo()}>Altera Dados</button>
         </div>
     )
 }
