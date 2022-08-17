269889fd62a423181605fb7f9bf76810cb88e4f5
viniciusreis <rm94707@fiap.com.br>
2022-08-17 09:43:03 -0300

    App Basico react + exemplo1 useState

---
+++ rm94707/aula03/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Repositorios from "./components/repositorios/Repositorios"
+
+// const App = () =>{
+//     return(
+//         <>
+//         </>
+//     )
+// }
+
+
+export default function App(){
+    return(
+        <div>
+            <h1>ESTUDO SOBRE HOOKS</h1>
+            <Repositorios/>
+        </div>
+    )
+}
