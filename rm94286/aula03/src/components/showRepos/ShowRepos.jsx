f052a4be0f4e349329bc8ba6557fd322ddf2b742
LeonardoRCosta <rm94286@fiap.com.br>
2022-08-17 12:03:16 -0300

    Erro a concertar.

--- rm94286/aula03/src/components/showRepos/ShowRepos.jsx
+++ rm94286/aula03/src/components/showRepos/ShowRepos.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function ShowRepos(props) {
-  let estilo;
-  if (new Date().getHours() < 12) {
-    estilo = props.objEstilos.manha;
-  } else if (new Date().getHours() > 12 && new Date().getHours() < 18) {
-    estilo = props.objEstilos.tarde;
-  } else {
-    estilo = props.objEstilos.noite;
-  }
+  useEffect(async () => {
+    const response = await fetch('https://api.github.com/users');
+    const data = await response.json();
+    props.setRepositorios(data);
+  }, []);
+
+  useEffect(() => {
+    console.log('Renderizou o Repositório!');
+  }, [props.repositorios]);
+
   return (
     <div>
-      <h2 style={estilo}>Lista de Repositórios</h2>
+      <h2>Lista de Repositórios</h2>
       <p>{props.repositorios}</p>
       <button onClick={() => props.setRepositorios('Alterado no filho!')}>
         Alterar
