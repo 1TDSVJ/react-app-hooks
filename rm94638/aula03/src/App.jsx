d607c3ffcee5256815420db53eb5ea4b09efce8a
TainaAlmd <rm94638@fiap.com.br>
2022-08-17 10:23:15 -0300

    criacao do app

---
+++ rm94638/aula03/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Repositorios from "./components/respositorios/Repositorios"
+
+export default function App(){
+    return(
+        <div>
+            <h1>ESTUDO SOBRE HOOKS</h1>
+            <Repositorios/>    
+        </div>
+    )
+}
+
+// const App = () =>{
+//     return(
+//         <>
+
+//         </>
+//     )
+// }
+
+// export default App
