f39d729d63c607355e8c6b751a1776764064de4c
LucasGuerraS <lucasgasardo@gmail.com>
2022-08-17 09:52:29 -0300

    ✨ New: arrow function to change repo

--- rm95084/aula03/src/components/repositorios/Repositorios.jsx
+++ rm95084/aula03/src/components/repositorios/Repositorios.jsx
@@ -1,14 +1,22 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const Repositorios = () => {
-    const [repositorios, setReposiorios] = useState()
+  const [repositorios, setReposiorios] = useState("1TDSVJ");
+  let valorVariavel = "2022"
+  const addRepo = () => {
+    setReposiorios(repositorios + 1);
+    valorVariavel += 5;
+    console.log(valorVariavel);
+  };
+
   return (
     <div>
-        <h2>Exemplo de utilização de useState</h2>
-        <p>Meu repositório : {repositorios}</p>
-        <button onClick={() => setReposiorios("1TDSVJ")}>Add-Repo</button>
+      <h2>Exemplo de utilização de useState</h2>
+      <p>Meu repositório : {repositorios}</p>
+      <p>Minha Variável: {valorVariavel}</p>
+      <button onClick={addRepo}>Add-Repo</button>
     </div>
-  )
-}
+  );
+};
 
-export default Repositorios
+export default Repositorios;
