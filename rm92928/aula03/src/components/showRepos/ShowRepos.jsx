6a82021d421c8c68c5466e19151136195ced991e
DiogoKahn <rm92928@fiap.com.br>
2022-08-18 09:10:38 -0300

    Att useEffect e async function

--- rm92928/aula03/src/components/showRepos/ShowRepos.jsx
+++ rm92928/aula03/src/components/showRepos/ShowRepos.jsx
@@ -1,13 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ShowRepos(props){
 
-    // a função é assíncrona para que ela continue carregando enquanto o resto da página carrega
-    useEffect(async () => {
-        const response = await fetch("https://api.github.com/users")
-        const data = await response.json() // a resposta vem como string, portanto .json() remonta os dados
-        props.setRepositorios(data)
-    },[])
+    const [newRepos, setnewRepos] = useState([]);
+
+    useEffect(() => {
+        carregaRepos()
+    }, [])
+
+    // Funções normais são síncronas (call back), esperam o retorno, ou seja, enquanto a função não tiver resposta (demorar demais) o programa não carrega
+    // a função é assíncrona para que a aplicação continue rodando enquanto a função não termina
+    const carregaRepos = async () => {
+        // await para evitar o timeout
+        const resp = await fetch("https://api.github.com/users/DiogoKahn/repos");
+        const data = await resp.json() // a resposta vem como string, portanto .json() remonta os dados
+        setnewRepos(data)
+    }
     
     // useEffeect executa o que estiver dentro dele quando o componente for renderizado
     // useEffect(()=>{
@@ -15,18 +23,18 @@ export default function ShowRepos(props){
     // },[])
 
     // como dependencias foram passadas, o useEffect só será executado quando o objeto for renderizado
-    useEffect(()=>{
-        console.log("RENDERIZOU O REPOSITORIO!")
-    },[props.repositorios])
+    // useEffect(()=>{
+    //     console.log("RENDERIZOU O REPOSITORIO!")
+    // },[props.repositorios])
     
     return(
         <div>
-
             <h2>Lista de Repositórios</h2>
-            <p>{props.repositorios}</p>
-            <button onClick={()=> props.setRepositorios("ALTERADO DO FILHO")}>ALTERAR</button>
-
-
+            <ul>
+               {newRepos.map((repositorio) =>
+                <li key={repositorio.id}>{repositorio.name}</li>
+               )}
+            </ul>
         </div>
     )
 }
