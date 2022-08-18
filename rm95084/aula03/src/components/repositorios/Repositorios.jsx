3552733ba38b517d11d4086143929a76551cb812
LucasGuerraS <lucasgasardo@gmail.com>
2022-08-18 08:37:58 -0300

    🚧 Update with master

--- rm95084/aula03/src/components/repositorios/Repositorios.jsx
+++ rm95084/aula03/src/components/repositorios/Repositorios.jsx
@@ -1,42 +1,42 @@
-import React, { useState } from "react";
-import ShowRepos from "../showRepos/ShowRepos";
+import React, {useState} from "react"
+import ShowRepos from "../showRepos/ShowRepos"
 
-const Repositorios = () => {
-  const objEstilos = {
-    noite: { backgroundColor: "#FF0000" },
-    tarde: { backgroundColor: "#FFFF00" },
-    manha: { backgroundColor: "#0000FF" },
-  };
-  const [repositorios, setRepositorios] = useState("1TDSVJ");
-  let valorVariavel = "2022";
-  const addRepo = () => {
-    setRepositorios(repositorios + 1);
-    valorVariavel += 5;
-    console.log(valorVariavel);
-  };
+export default function Repositorios(){
+    
+    const objEstilos = {
+        noite:{backgroundColor:'#FF0000'},
+        tarde:{backgroundColor:'#FFF000'},
+        manha:{backgroundColor:'#0000FF'}
+    }
 
-  return (
-    <div>
-      <h2
-        style={{
-          backgroundColor: "#00FF00",
-          color: "#FF0000",
-          textAlign: "center",
-        }}
-      >
-        Exemplo de utilização de useState
-      </h2>
-      <p style={objEstilos.noite}>Meu repositório : {repositorios}</p>
-      <p style={objEstilos.manha}>Minha Variável: {valorVariavel}</p>
-      <button onClick={addRepo}>Add-Repo</button>
 
-      <ShowRepos
-        repositorios={repositorios}
-        setRepositorios={setRepositorios}
-        objEstilos={objEstilos}
-      />
-    </div>
-  );
-};
 
-export default Repositorios;
+    const [repositorios, setRepositorios] = useState([])
+    let valorVariavel = "2022"
+
+    const alteraRepo = () =>{
+        setRepositorios(repositorios + 1)
+        valorVariavel += 5
+        console.log(valorVariavel)
+    }
+
+    return(
+
+        <div>
+            <h2 style={{backgroundColor:'#00FF00',color:'#FF0000', textAlign:'center'}}>Exemplo de utilização do useState</h2>
+            <p style={objEstilos.noite}>Meu repositório : {repositorios}</p>
+            <p style={objEstilos.manha}>Minha Variável: {valorVariavel}</p>
+
+            <button onClick={()=> alteraRepo()}>Altera Dados</button>
+
+            <ShowRepos 
+                repositorios={repositorios}
+                setRepositorios={setRepositorios}
+                objEstilos={objEstilos}
+                />
+            
+
+        </div>
+
+    )
+}
