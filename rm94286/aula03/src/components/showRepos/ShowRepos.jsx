95714e0c7173bce80c37c4b795c8dbbf928b05c5
LeonardoRCosta <rm94286@fiap.com.br>
2022-08-18 09:52:22 -0300

    Componente ShowRepos finalizado.

--- rm94286/aula03/src/components/showRepos/ShowRepos.jsx
+++ rm94286/aula03/src/components/showRepos/ShowRepos.jsx
@@ -5,7 +5,8 @@ export default function ShowRepos() {
 
   useEffect(() => {
     carregaRepos();
-  });
+  },[]); // Quando colocamos esses colchetes de dependência, ele vai executar o useEffect apenas uma vez, quando o componente for montado.
+         // O famoso componentDidMount.
 
   const carregaRepos = async () => {
     const resp = await fetch(
@@ -19,9 +20,7 @@ export default function ShowRepos() {
     <div>
       <h2>Lista de Repositórios</h2>
       <ul>
-        {newRepos.map((repositorio, i) => (
-          <li key={i}>{repositorio.name}</li>
-        ))}
+        {newRepos.map((repositorio, i) => <li key={repositorio.id}>{repositorio.name}</li>)}
       </ul>
     </div>
   );
