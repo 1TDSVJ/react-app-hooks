cdb82551f2f08af9203ffd5c4b0043da11b6ae25
heitorborbamarini <rm92976@fiap.com.br>
2022-08-18 09:43:21 -0300

    adicionando github users

--- rm92976/aula03/src/App.jsx
+++ rm92976/aula03/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Repositorios from "./components/repositorios/Repositorios";
 import { useEffect } from "react"
+import { useState } from "react"
+import GithubUsers from "./components/githubUsers/GithubUsers";
 
 
 export default function App() {
   return (
     <div>
         <h1>ESTUDOS SOBRE HOOKS </h1>
-        <Repositorios/>
+        <GithubUsers/>
 
     </div>
   );
