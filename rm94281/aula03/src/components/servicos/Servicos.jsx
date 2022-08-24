38d2040326bae37a5fd4726e7e81b87af98f7a63
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-24 09:50:03 -0300

    Componente Serviços finalizado.

---
+++ rm94281/aula03/src/components/servicos/Servicos.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Servicos() {
+  const servicos = {
+    backgroundColor: '#bfb',
+    height: '85vh',
+    textAlign: 'center',
+    color: '#7c7',
+  };
+
+  return (
+    <div style={servicos}>
+      <h1>Página Serviços</h1>
+      <p>Exemplo de página de serviços do projeto</p>
+    </div>
+  );
+}
