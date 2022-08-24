95d50b9e1bd22ede0b581583b8bb957b47b062b0
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-24 16:19:01 -0300

    Componente HomeLoja finalizado.

--- rm94281/aula03/src/ex-loja-moda/home-loja/HomeLoja.jsx
+++ rm94281/aula03/src/ex-loja-moda/home-loja/HomeLoja.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import img from '../img/loja.jpg'
 export default function HomeLoja() {
   return (
-    <main>
+    <main style={{textAlign: 'center', fontFamily:'Courier'}}>
       <h1>Seja bem vindo a Loja Moda!</h1>
       <p>A loja dos seus sonhos...</p>
+      <figure>
+        <img src={img} alt="Loja de roupa" style={{width:'30vw'}}/>
+      </figure>
     </main>
   );
 }
