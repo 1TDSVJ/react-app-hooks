881662e3b88b628fc0f68b4ead8c04f2b023ff59
heitorborbamarini <rm92976@fiap.com.br>
2022-08-24 12:01:18 -0300

    desafio a fazer

--- rm92976/aula03/src/desafio/menu-loja/Menuloja.jsx
+++ rm92976/aula03/src/desafio/menu-loja/Menuloja.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import{Link} from "react-router-dom";
 
 
+
+
 export default function Menuloja(){
 
     
 
     return(
         <header>
-            <nav>
-                <ul>
-                    <li><Link to='/'> Home</Link></li>
+            <nav style={menucss.nav}>
+                <ul style={menu.css.ul}>
+                    <li ><Link to='/'> Home</Link></li>
                     <li><Link to='/modaFeminina'> Moda Feminina</Link></li>
                     <li><Link to='/modaMasculina'> Moda Masculina</Link></li>
                     <li><Link to='/modaInfantil'> Moda Infantil</Link></li>
