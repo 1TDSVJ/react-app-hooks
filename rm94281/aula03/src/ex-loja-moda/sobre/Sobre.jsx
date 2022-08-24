134164b3e33784a4a5c8acef4cceb531635c9137
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-24 16:20:59 -0300

    Componente Sobre concluído

--- rm94281/aula03/src/ex-loja-moda/sobre/Sobre.jsx
+++ rm94281/aula03/src/ex-loja-moda/sobre/Sobre.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Sobre() {
   return (
-    <section>
+    <section style={{padding: '2rem 5rem', fontFamily: 'Courier'}}>
       <h1>Sobre nós...</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio
@@ -16,6 +16,34 @@ export default function Sobre() {
         Minus, animi officia ab modi reprehenderit cumque? Quaerat quas dicta
         laboriosam adipisci repellat ut?
       </p>
+      <h2>Como começamos?</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio
+        tempore vitae sequi quo, repellendus delectus illum amet eaque?
+        Delectus, beatae? Corporis vero praesentium error incidunt molestias
+        quisquam magni ea! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Soluta natus dicta sunt hic est placeat, ab explicabo quia quas
+        nobis ipsa corrupti perferendis dolorum possimus accusamus alias,
+        debitis dignissimos. Et. Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Vero tempore corporis tempora, nostrum et eius ut?
+        Minus, animi officia ab modi reprehenderit cumque? Quaerat quas dicta
+        laboriosam adipisci repellat ut?
+      </p>
+      <h2>Nosso diferencial:</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio
+        tempore vitae sequi quo, repellendus delectus illum amet eaque?
+        Delectus, beatae? Corporis vero praesentium error incidunt molestias
+        quisquam magni ea! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Soluta natus dicta sunt hic est placeat, ab explicabo quia quas
+        nobis ipsa corrupti perferendis dolorum possimus accusamus alias,
+        debitis dignissimos. Et. Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Vero tempore corporis tempora, nostrum et eius ut?
+        Minus, animi officia ab modi reprehenderit cumque? Quaerat quas dicta
+        laboriosam adipisci repellat ut?
+      </p>
+      <h3>Desenvolvido por:</h3>
+      <p>RafaRamosCosta RM: 94281</p>
     </section>
   );
 }
