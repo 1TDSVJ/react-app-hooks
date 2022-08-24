2219a78437b39e2881e907896dc702c9e667cb25
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-24 09:56:55 -0300

    resolvido

--- pf0670/aula03/src/components/menu/Menu.jsx
+++ pf0670/aula03/src/components/menu/Menu.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import {Link} from 'react-router-dom'
 export default function Menu() {
@@ -16,32 +15,3 @@ export default function Menu() {
 
   )
 }
-=======
-import React from "react";
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
-
-
-
-export default function Menu() {
-    return(
-      <header>
-           <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/produtos">Produtos</Link></li>
-                    <li><Link to="/servicos">Servicos</Link></li>
-             </ul>
-     
-      </header>
-
-
-      
-
-
-
-)
-
-
-
-
-}
->>>>>>> ddc76b05ba86b9b35ed9f142cb7a75316c68eeec
--- pf0670/aula03/src/index.js
+++ pf0670/aula03/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom' 
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-<<<<<<< HEAD
 
 import './index.css'
 ReactDOM.render(
@@ -10,16 +9,3 @@ ReactDOM.render(
         <App/>
     </BrowserRouter>
 , document.getElementById("root"))
-
-
-=======
-import './index.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
-
-ReactDOM.render(
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
- , document.getElementById("root"))
->>>>>>> ddc76b05ba86b9b35ed9f142cb7a75316c68eeec
