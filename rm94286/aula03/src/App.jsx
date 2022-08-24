d04d326dbfa6ddc05122e68b4883afde28f6efc5
LeonardoRCosta <rm94286@fiap.com.br>
2022-08-24 09:48:42 -0300

    Menu e rotas para outros componentes add.

--- rm94286/aula03/src/App.jsx
+++ rm94286/aula03/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import GithubUsers from './components/githubUsers/GithubUsers';
-import Repositorios from './components/repositorios/Repositorios';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Produtos from './components/produtos/Produtos';
+import Servicos from './components/servicos/Servicos';
+import Menu from './menu/Menu';
 
 export default function App() {
   return (
     <>
-      <h1>Estudos sobre HOOKS</h1>
-      <Repositorios />
-      <GithubUsers />
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/produtos" element={<Produtos />} />
+        <Route path="/servicos" element={<Servicos />} />
+      </Routes>
     </>
   );
 }
