9aa30cc8aba2b252d7f9b02131649831e0121a2a
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-24 16:18:49 -0300

    Finalizei o componente MenuLoja.

--- rm94281/aula03/src/ex-loja-moda/menu-loja/MenuLoja.jsx
+++ rm94281/aula03/src/ex-loja-moda/menu-loja/MenuLoja.jsx
@@ -2,27 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function MenuLoja() {
-  const menuStyles = {
-    color: 'white',
+  const headerStyles = {
+    backgroundColor: 'purple',
+    padding:'1rem',
+    fontFamily: 'Monospace',
+  }
+  const ulStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    listStyle: 'none',
   };
+  const linkStyles = {
+    textDecoration: 'none',
+    color: 'white',
+    fontSize: '18px',
+    textTransform: 'uppercase'
+  }
   return (
-    <header>
+    <header style={headerStyles}>
       <nav>
-        <ul style={menuStyles}>
+        <ul style={ulStyles}>
           <li>
-            <Link to="/">HOME</Link>
+            <Link to="/" style={linkStyles}>HOME</Link>
           </li>
           <li>
-            <Link to="/modamasculina">Moda Masculina</Link>
+            <Link to="/modamasculina" style={linkStyles}>Moda Masculina</Link>
           </li>
           <li>
-            <Link to="/modafeminina">Moda Feminina</Link>
+            <Link to="/modafeminina" style={linkStyles}>Moda Feminina</Link>
           </li>
           <li>
-            <Link to="/modainfantil">Moda Infantil</Link>
+            <Link to="/modainfantil" style={linkStyles}>Moda Infantil</Link>
           </li>
           <li>
-            <Link to="/sobre">Sobre</Link>
+            <Link to="/sobre" style={linkStyles}>Sobre</Link>
           </li>
         </ul>
       </nav>
