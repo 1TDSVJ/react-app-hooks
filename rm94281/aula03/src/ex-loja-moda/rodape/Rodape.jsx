1f6ec30dcf397a850c1bd442a6e72a9fcb06d48f
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-24 16:20:48 -0300

    Componente Rodape finalizado.

--- rm94281/aula03/src/ex-loja-moda/rodape/Rodape.jsx
+++ rm94281/aula03/src/ex-loja-moda/rodape/Rodape.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 export default function Rodape() {
+  const footerStyles = {
+    textAlign: 'center',
+    backgroundColor: 'purple',
+    width: '100%',
+    fontFamily: 'Monospace'
+  }
   return (
-    <footer>
-      <p>Fala conosco</p>
+    <footer style={footerStyles}>
+      <h2>Conheça nossa loja física!</h2>
+      <p style={{fontSize: '1rem'}}>Av. Paulista, 1313</p>
+      <h2>Entre em contato:</h2>
       <p>tel: (11)99999-9999</p>
       <p>email: xxxx@gmail.com</p>
+      <p>Desenvolvido por: Rafael Ramos Costa - RM: 94281</p>
+      <p>Todos os direitos reservados, Loja Moda 2022&copy;</p>
     </footer>
   );
 }
