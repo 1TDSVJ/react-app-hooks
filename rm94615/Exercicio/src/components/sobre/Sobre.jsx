ca40929967e110cfd58ef2f180edc514457a7b86
DaniHenriqueAOM <daniel.h.a.o.m@gmail.com>
2022-08-25 08:25:30 -0300

    Exercício finalizado.

--- rm94615/Exercicio/src/components/sobre/Sobre.jsx
+++ rm94615/Exercicio/src/components/sobre/Sobre.jsx
@@ -43,44 +43,6 @@ export default function Sobre(props) {
             </figcaption>
         </figure>
 
-        <h1>Grupo:</h1>
-        
-        <figure style={estiloGrupo}>
-            <img src={props.fotoProps[1]} alt="Avatar do Github" />
-            <figcaption>
-                <p>{props.nomeProps[1]}</p>
-                <a href={props.gitProps[1]} style={{textDecoration: 'none'}}>{props.nicknameProps[1]}</a>
-                <p>{props.turmaProps[1]}</p>
-            </figcaption>
-        </figure>
-
-        <figure style={estiloGrupo}>
-            <img src={props.fotoProps[2]} alt="Avatar do Github" />
-            <figcaption>
-                <p>{props.nomeProps[2]}</p>
-                <a href={props.gitProps[2]} style={{textDecoration: 'none'}}>{props.nicknameProps[2]}</a>
-                <p>{props.turmaProps[2]}</p>
-            </figcaption>
-        </figure>
-
-        <figure style={estiloGrupo}>
-            <img src={props.fotoProps[3]} alt="Avatar do Github" />
-            <figcaption>
-                <p>{props.nomeProps[3]}</p>
-                <a href={props.gitProps[3]} style={{textDecoration: 'none'}}>{props.nicknameProps[3]}</a>
-                <p>{props.turmaProps[3]}</p>
-            </figcaption>
-        </figure>
-
-        <figure style={estiloGrupo}>
-            <img src={props.fotoProps[0]} alt="Avatar do Github" />
-            <figcaption>
-                <p>{props.nomeProps[0]}</p>
-                <a href={props.gitProps[0]} style={{textDecoration: 'none'}}>{props.nicknameProps[0]}</a>
-                <p>{props.turmaProps[0]}</p>
-            </figcaption>
-        </figure>
-        {/**/}
         </>
     )
 }
