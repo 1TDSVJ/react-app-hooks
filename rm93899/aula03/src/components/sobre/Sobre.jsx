100f11256b0af42f65c15d9049c7e46d2b0238d2
EmersonGald1n0 <rm93899@fiap.com.br>
2022-08-25 23:39:17 -0300

    Add sobre

---
+++ rm93899/aula03/src/components/sobre/Sobre.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const sobre = {
+  backgroundColor: 'gray',
+  height: '20vh',
+  textAlign: 'center',
+  color:'black'
+  }
+
+function Sobre() {
+  return (
+    <div style={sobre}>
+        <h1>Integrantes</h1>
+        <p>Marco Martinez - RM95614</p>
+    </div>
+  )
+}
+
+export default Sobre;
