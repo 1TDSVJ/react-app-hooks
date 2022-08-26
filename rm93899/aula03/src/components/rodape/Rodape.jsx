561e71f5b2daa87269d4e35e783b9eb09f40c6dc
EmersonGald1n0 <rm93899@fiap.com.br>
2022-08-25 23:39:30 -0300

    Add rodapé

---
+++ rm93899/aula03/src/components/rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Container } from './styles';
+const rodape = {
+  backgroundColor: '#f5f5',
+  
+  }
+export default function Rodape() {
+  return (
+    <div style={rodape}>
+        <p>Desenvolvido por ArtLabs - Todos os direitos reservados - 2022</p>
+    </div>
+  )
+}
