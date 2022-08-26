4f61372abaaa5e05e5b2e6e3f93b85c90330d76c
EmersonGald1n0 <rm93899@fiap.com.br>
2022-08-25 23:39:58 -0300

    Add moda infantil

---
+++ rm93899/aula03/src/components/modaInfantil/ModaInfantil.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const modaInfantil = {
+  backgroundColor: '#fc0',
+  height: '20vh',
+  textAlign: 'center',
+  color:'black'
+  }
+function ModaInfantil() {
+  return (
+    <div style={modaInfantil}>
+        <h1>Moda Infantil</h1>
+        <img src="" alt="" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nobis quo ipsam temporibus, sapiente alias, natus, doloremque facere ullam aliquam deserunt distinctio dicta? Sequi nostrum a nihil ut quasi cum.</p>
+    </div>
+  )
+}
+
+export default ModaInfantil;
