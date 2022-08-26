ec732aea982cd16cfb2fa026b491c28cc843bdb4
EmersonGald1n0 <rm93899@fiap.com.br>
2022-08-25 23:39:45 -0300

    Add moda masculina

---
+++ rm93899/aula03/src/components/modaMasculina/ModaMasculina.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const modaMasculina = {
+backgroundColor: '#fb7',
+height: '20vh',
+textAlign: 'center',
+color:'black'
+}
+function ModaMasculina() {
+  return (
+  <div style={modaMasculina}>
+    <h1>Moda Masculina</h1>
+    <img src=".'" alt="" />
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima ut animi rem, veniam fuga dolores reprehenderit aperiam quae doloremque odio, assumenda voluptatibus recusandae nam facilis porro pariatur suscipit ex iure!</p>
+  </div>
+  )
+}
+
+export default ModaMasculina;
